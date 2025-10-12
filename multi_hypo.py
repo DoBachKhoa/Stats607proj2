7273d9c3b5c7da38934610cb2db617bf94a2e237
DoBachKhoa <khoadb@l-jlg2wvvnlx.local>
2025-10-12 17:11:29 -0400

    Add multi hypothesis classes

---
+++ multi_hypo.py
@@ -0,0 +1,54 @@
+import math
+import numpy as np
+
+class MultipleHypotheses:
+    '''
+    Base multiple hypothesis class
+    '''
+    def __init__(self, num_hypo=1, num_nul=1):
+        assert num_hypo >= num_null, \
+            "Number of null hypotheses cannot surpass number of hypotheses"
+        self.num_hypo = num_hypo
+        self.num_null = num_null
+        self.num_alt  = num_hypo - num_null
+
+    def generate_data(self, size=None):
+        raise NotImplementedError
+
+    def generate_p_values(sefl, test):
+        return test(self.generate_data())
+
+class StandardNullHypotheses(MultipleHypotheses):
+    '''
+    Simple uniform hypothesis family
+    Generated data can also be thought of as p values under the null
+    '''
+    def __init__(self, num_hypo=1):
+        super().__init__(self, num_hypo=num_hypo, num_null=0)
+        
+    def generate_data(self, size=None)
+        if shape == None: return np.random.uniform(self.num_hypo)
+        else: return np.random.uniform(size = (size, self.num_hypo))
+
+class NormalMeanHypotheses(MultipleHypotheses):
+    '''
+    Similar-variance 1D Normal distribution hypotheses
+    The null indicates that the mean is 0
+    '''
+    def __init__(self, num_null, alt_means, sigma=1.):
+        num_hypo = num_null + len(alt_means)
+        super().__init__(num_hypo, num_null)
+        self.means = np.array([0.]*num_null+list(alt_means))
+        self.sigma = sigma
+
+    def generate_data(self, size=None):
+        if shape == None: return np.random.normal(self.means, self.sigma)
+        else: return np.random.normal(self.means, shape=(size, self.num_hypo))
+
+
+
+
+
+    
+
+    
